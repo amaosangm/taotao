01a5176441a2c38e9de60e25dd51d7e7b62bd836
amao <252620356@qq.com>
2019-06-02 12:01:59 +0800

    this is first commit

---
+++ taotao-manager/taotao-manager-dao/src/main/java/com/taotao/mapper/TestMapper.java
@@ -0,0 +1,11 @@
+package com.taotao.mapper;
+
+/**
+ * 测试接口查询当前的时间
+ * @author amao
+ *
+ */
+public interface TestMapper {
+	
+	public String queryNow();
+}
---
+++ taotao-manager/taotao-manager-interface/src/main/java/com/taotao/service/TestService.java
@@ -0,0 +1,12 @@
+package com.taotao.service;
+
+/**
+ * 测试接口 查询当前时间
+ * @author amao
+ *
+ */
+public interface TestService {
+	
+	//查询当前的时间
+	public String queryNow();
+}
